6ccf0e68b8463ea3a238f20559d159d006e6ebd7
JandersonMota00 <20231tadssaj@ifba.edu.br>
2025-03-02 15:48:50 -0300

    Nova versão do projeto por ter alterado o JDK para a versão 21

---
+++ agencia-viagens/src/test/java/com/agenciaviagens/agencia_viagens/AgenciaViagensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenciaviagens.agencia_viagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaViagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
