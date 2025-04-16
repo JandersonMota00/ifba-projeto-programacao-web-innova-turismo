39949c0b8b7cea662adb76bc619b8f53a17bdbb9
JandersonMota00 <20231tadssaj@ifba.edu.br>
2025-04-16 10:09:01 -0300

    Coloquei a tag <button> dentro da tag <form>

--- agencia-viagens/src/main/resources/static/js/alternar-status.js
+++ agencia-viagens/src/main/resources/static/js/alternar-status.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll(".ativo").forEach(button => {
         button.addEventListener("click", function() {
-            if (this.textContent === "Ativar") {
-                this.textContent = "Desativar";
+            if (this.textContent === "Ativado") {
+                this.textContent = "Desativado";
                 this.style.backgroundColor = "#e94e1b";
             } else {
-                this.textContent = "Ativar";
+                this.textContent = "Ativado";
                 this.style.backgroundColor = ""; // Volta ao padrão
             }
         });
