edf8f0d3b911654a2431ffa3abfaa3e78d468912
Amandalopes28 <136410653+amandalopes2@users.noreply.github.com>
2025-04-21 00:45:59 -0300

    entity do pacote viagem

--- agencia-viagens/src/main/java/com/agenciaviagens/model/Viagem.java
+++ agencia-viagens/src/main/java/com/agenciaviagens/model/Viagem.java
@@ -2,20 +2,25 @@ package com.agenciaviagens.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-
-import jakarta.persistence.Column;
+import jakarta.persistence.*;// Inclui Entity, Table, Id, Column, etc.
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
+@Entity
+@Table(name = "viagens")
 @Getter
 @Setter
 @NoArgsConstructor
 public class Viagem {
  
- 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @NotNull
     @Size(min = 3, max = 255)
     @Column(name = "titulo",unique = true)
